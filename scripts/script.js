471aea5880b6122068420f7b6c49f67a1110f8e1
William-Dias-Marcos <william.diasmarcos@gmail.com>
2022-01-19 21:29:49 -0300

    Part 02

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,35 @@
-// dólar
-let url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+const url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
 
-    fetch(url)
-    .then((res) => {
+    fetch(url).then((res) => {
 
         return res.json()
 
     })
     .then((data) => {
 
-        console.log(data.USDBRL.ask)
+        // dólar
         let USD = data.USDBRL.ask
+        document.getElementById("usd").innerHTML = `R$ ${Number(USD).toLocaleString("pt-BR", {
+            minimumFractionDigits: 2,  
+            maximumFractionDigits: 2
+         })}`
 
-        document.getElementById("usd").innerHTML = `R$ ${Number(USD).toFixed(2)} `
-        
+        // euro
+        let EUR = data.EURBRL.ask
+        document.getElementById("eur").innerHTML = `R$ ${Number(EUR).toLocaleString("pt-BR", {
+            minimumFractionDigits: 2,  
+            maximumFractionDigits: 2
+         })}`
+
+        // bitcoin
+        let BTC = data.BTCBRL.ask
+        document.getElementById("btc").innerHTML = `R$ ${Number(BTC).toFixed(3)},00`
     })
+
+    
+
+
+    
     
    
 
