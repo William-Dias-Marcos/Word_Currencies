a48261345266b36309476993b40425d62d64f3ff
William Dias Marcos <william.diasmarcos@gmail.com>
2022-04-27 09:36:05 -0300

    Part 08

--- scripts/script.js
+++ scripts/script.js
@@ -9,21 +9,21 @@ const url = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
 
         console.log(data)
         // dólar
-        let USD = data.USD.ask
+        let USD = data.USDBRL.ask
         document.getElementById("usd").innerHTML = `R$ ${Number(USD).toLocaleString("pt-BR", {
             minimumFractionDigits: 2,  
             maximumFractionDigits: 2
          })}`
 
         // euro
-        let EUR = data.EUR.ask
+        let EUR = data.EURBRL.ask
         document.getElementById("eur").innerHTML = `R$ ${Number(EUR).toLocaleString("pt-BR", {
             minimumFractionDigits: 2,  
             maximumFractionDigits: 2
          })}`
 
         // bitcoin
-        let BTC = data.BTC.ask
+        let BTC = data.BTCBRL.ask
         document.getElementById("btc").innerHTML = `R$ ${Number(BTC).toFixed(3)},00`
 
     
